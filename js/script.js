d5af8f0f4aa78926a0f6c3ef5bcad8553b235f0a
simeon <simeonpitts@gmail.com>
2023-07-08 21:57:00 -0700

    i have fixed the innertext lables so now it should pop up the answers i have sulected

--- js/script.js
+++ js/script.js
@@ -1,40 +1,42 @@
-// busniss logic
-
 function numberfinder() {
-    const number1Push = document.getElementById("numberPush").value 
+    const number1Push = document.getElementById("numberPush").value;
     let wordarry = [];
-    for (let index = 0; index <= number1Push; index++){
-        let string = index.toString();
-        if (string.includes("1")) {
-            wordarry.push("beep!");
-        } else if (string.includs("2")) {
-            wordarry.push("boop!");
-        } else if (string.includes("3")) {
-            wordarry.push("wont you be my neighber?");
-        } else {
-            wordarry.push(index);
-        }
+    for (let index = 0; index <= number1Push; index++) {
+      let string = index.toString();
+      if (string.includes("1")) {
+        wordarry.push("beep!");
+      } else if (string.includes("2")) {
+        wordarry.push("boop!");
+      } else if (string.includes("3")) {
+        wordarry.push("won't you be my neighbor?");
+      } else {
+        wordarry.push(index);
+      }
     }
     const finalewordcount = wordarry.join(", ");
     return finalewordcount;
-}
-
-// UI logic 
-
-window.onload = function() {
-    const form = document.querySelector ("form");
-    form.onsubmit = function(event) {
-        event.preventDefault();
-        let number1push = document.getElementById("numberPush").value;
-        if (number1push === ""){
-            document.getElementById("answer").innerText = numberfinder();
-        } else {
-            document.getElementById("answer").innerText = "we need a number";
-        }
-        return number1push;
-        
-    }
-}
+  }
+  
+  // UI logic
+  
+  window.onload = function () {
+    const form = document.querySelector("form");
+    form.onsubmit = function (event) {
+      event.preventDefault();
+      let number1push = document.getElementById("numberPush").value;
+      const answerParagraph = document.querySelector(".answer");
+      if (!answerParagraph) {
+        console.error("Answer element not found.");
+        return;
+      }
+      if (number1push === "") {
+        answerParagraph.innerText = numberfinder();
+      } else {
+        answerParagraph.innerText = "We need a number.";
+      }
+      return number1push;
+    };
+  };
 
 
 
